8ac2948517720682c409469853e9ea7213b66493
wangjue <wangjue1199@163.com>
2017-12-20 18:13:47 +0800

    add hemi-sphere

--- js/three.js
+++ js/three.js
@@ -5,6 +5,8 @@ const threeLayer = new maptalks.ThreeLayer('t', {
     forceRenderOnZooming: true
 });
 
+let SCENE = null;
+
 const materials = (function () {
     const materials = [];
     const loader = new THREE.TextureLoader();
@@ -13,6 +15,7 @@ const materials = (function () {
         texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
         texture.repeat.set(0.002, 0.002);
         materials.push(new THREE.MeshPhongMaterial({
+            blending: THREE.AdditiveBlending,
             map: texture
         }));
     }
@@ -26,6 +29,7 @@ xhttp.onreadystatechange = function () {
         const features = JSON.parse(this.responseText);
 
         threeLayer.prepareToDraw = function (gl, scene, camera) {
+            SCENE = scene;
             const me = this;
             const light = new THREE.PointLight(0xffffff);
             camera.add(light);
@@ -50,7 +54,7 @@ xhttp.onreadystatechange = function () {
                     scene.add(mesh);
                 }
             });
-            map.setPitch(55);
+            refresh();
         };
         threeLayer.addTo(map);
     }
@@ -58,4 +62,49 @@ xhttp.onreadystatechange = function () {
 xhttp.open("GET", "./data/deck_buildings.json", true);
 setTimeout(() => {
     xhttp.send();
+
+    setTimeout(() => {
+        drawElements();
+    }, 3000);
 }, 2000);
+
+function drawElements() {
+    if (!SCENE) {
+        console.warn('three.js scene is empty');
+        return;
+    }
+
+    // hemi-sphere
+    const hemi_sphere_geometry = new THREE.SphereGeometry(1600, 32, 32);
+    const hemi_sphere_material = new THREE.MeshPhongMaterial({
+        color: 0x806666,
+        transparent: true,
+        opacity: 0.2,
+        clippingPlanes: [new THREE.Plane(new THREE.Vector3(0, 0, -1))]
+    });
+    const hemi_sphere = new THREE.Mesh(hemi_sphere_geometry, hemi_sphere_material);
+    hemi_sphere.position.set(-27589453.947005168, -16645787.262404276, 0);
+    SCENE.add(hemi_sphere);
+
+    // circle-plane
+    const circle_plane_geometry = new THREE.CircleGeometry(1600, 32);
+    const circle_plane_material = new THREE.MeshBasicMaterial({
+        color: 0x220000,
+        opacity: 0.1,
+        side: THREE.BackSide
+    });
+    const circle_plane = new THREE.Mesh(circle_plane_geometry, circle_plane_material);
+    circle_plane.position.set(-27589453.947005168, -16645787.262404276, 0);
+    SCENE.add(circle_plane);
+
+    // add spot light
+    const spotLightGeometry = new THREE.CylinderGeometry(0.1, 1.5, 5, 32 * 2, 20, true);
+    // geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, -geometry.parameters.height / 2, 0));
+    // geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+    refresh();
+}
+
+function refresh() {
+    map.setPitch(55 + 0.1 * Math.random());
+}
